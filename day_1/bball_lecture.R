5d613ad1f389a6024382347582f930ccf33c0f6d
Kyle McWagner <wagnerk1@uci.edu>
2021-08-24 14:03:04 -0700

    bball

---
+++ day_1/bball_lecture.R
@@ -0,0 +1,28 @@
+library(ggplot2) # explain base verses packages
+
+pointlow<-c(3,5,6,8,9,1,4) # Make a vector by hand, objects and simple functions
+
+pointhigh<-sample(10:20,7) # More complex functions with arguments
+
+points<-c(pointlow,pointhigh) # data manipulation
+points<-as.data.frame(points)
+
+# Non numaric objects
+hight <- c("short","short","short","short","short","short","short","tall","tall","tall","tall","tall","tall","tall") 
+hight <- c(rep("short", 7),rep("tall", 7))
+
+# data manipulation
+hight<- as.data.frame(hight) 
+bball<-cbind(points,hight)
+View(bball)
+
+
+#Graphing
+boxplot(points~hight, data = bball) 
+ggplot(bball, aes(x=hight, y=points, fill=hight)) +
+  geom_boxplot()
+
+
+t.test(points~hight, data=bball)
+mod.0 <-lm(points~hight, data=bball)
+summary(mod.0)
